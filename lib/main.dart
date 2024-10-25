daba1fc07dae48bc92f45afefe369058437983c4
okadwaja <okadwaja99@gmail.com>
2024-10-25 23:05:38 +0800

    menambahkan avatar3 & update tab

--- lib/main.dart
+++ lib/main.dart
@@ -29,18 +29,60 @@ class WhatsAppHomePage extends StatelessWidget {
       length: 3, // Jumlah Tab
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('WhatsApp'),
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              color: Colors.green, // Menambahkan warna hijau pada teks
+            ),
+          ),
           actions: [
             IconButton(icon: const Icon(Icons.camera_alt), onPressed: () {}),
             IconButton(icon: const Icon(Icons.search), onPressed: () {}),
             IconButton(icon: const Icon(Icons.more_vert), onPressed: () {}),
           ],
-          bottom: const TabBar(
+          bottom: TabBar(
             tabs: [
-              Tab(text: "All"),
-              Tab(text: "Unread"),
-              Tab(text: "Groups"),
+              Tab(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child:
+                      const Text("All", style: TextStyle(color: Colors.green)),
+                ),
+              ),
+              Tab(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: const Text("Unread",
+                      style: TextStyle(color: Colors.black)),
+                ),
+              ),
+              Tab(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: const Text("Groups",
+                      style: TextStyle(color: Colors.black)),
+                ),
+              ),
             ],
+            indicatorColor: Colors
+                .transparent, // Mengubah warna garis bawah tab menjadi transparan
+            labelColor: Colors
+                .green, // Mengubah warna teks tab yang aktif menjadi hijau
+            unselectedLabelColor:
+                Colors.black, // Mengubah warna teks tab yang tidak aktif
           ),
         ),
         body: const TabBarView(
@@ -64,10 +106,36 @@ class ChatListView extends StatelessWidget {
       children: const [
         ListTile(
           leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/avatar3.png'),
+          ),
+          title: Text('Anton'),
+          subtitle: Text('Sampai Jumpa...'),
+          trailing: Column(
+            mainAxisSize:
+                MainAxisSize.min, // Mengatur ukuran kolom sesuai dengan konten
+            crossAxisAlignment:
+                CrossAxisAlignment.end, // Mengatur agar kolom rata kanan
+            children: [
+              Text('Yesterday'),
+              Icon(
+                Icons.push_pin, // Ganti dengan ikon pin yang sesuai
+                color: Colors.black,
+                size: 18.0, // Mengatur warna ikon
+              ),
+            ],
+          ),
+        ),
+        ListTile(
+          leading: CircleAvatar(
             backgroundImage: AssetImage('assets/avatar1.png'),
           ),
           title: Text('Andrius Schneider'),
-          subtitle: Text('Typing...'),
+          subtitle: Text(
+            'Typing...',
+            style: TextStyle(
+              color: Colors.green, // Menambahkan warna hijau pada teks
+            ),
+          ),
           trailing: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
